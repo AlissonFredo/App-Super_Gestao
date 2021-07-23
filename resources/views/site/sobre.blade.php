cf0919f506bbec13b83187fa390848e825f9de35
alisson fredo <douglasalissonsf@gmail.com>
2021-07-23 18:19:39 -0300

    seção 6 - avançando com rotas (Routes)

--- resources/views/site/sobre.blade.php
+++ resources/views/site/sobre.blade.php
@@ -1,1 +1,13 @@
 <h3>Sobre (view)</h3>
+
+<ul>
+    <li>
+        <a href="{{ route('site.index') }}">Principal</a>
+    </li>
+    <li>
+        <a href="{{ route('site.sobre') }}">Sobre</a>
+    </li>
+    <li>
+        <a href="{{ route('site.contato') }}">Contato</a>
+    </li>
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -31,8 +31,17 @@ Route::get('/contato', function () {
 });
 */
 
-Route::get('/', [PrincipalController::class, 'principal']);
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobre', [SobreController::class, 'sobre'])->name('site.sobre');
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', function(){return 'login';})->name('site.login');
 
-Route::get('/sobre', [SobreController::class, 'sobre']);
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){return 'clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function(){return 'fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function(){return 'produtos';})->name('app.produtos');
+});
 
-Route::get('/contato', [ContatoController::class, 'contato']);
+Route::fallback(function(){
+    echo 'A rota não existe, <a href="'.route('site.index').'">Clique aqui</a> para ir a pagina inicial';
+});
