27e201143cd293bbd3c16c23ca01da6c81e1439c
alisson fredo <douglasalissonsf@gmail.com>
2021-07-27 01:26:49 -0300

    seção 7 - Avançando com controladores (Controllers) e visualizações (views)

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,9 @@
+<h1>entrou view fornecedor</h1>
+
+@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados</h3>
+@elseif(count($fornecedores) > 10)
+    <h3>Existem varios fornecedores cadastrados</h3>
+@else
+    <h3>Ainda não existem fornecedores cadastrados</h3>
+@endif
--- routes/web.php
+++ routes/web.php
@@ -4,41 +4,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\SobreController;
 use App\Http\Controllers\ContatoController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-/*
-Route::get('/', function () {
-    return 'app super gestao raiz';
-});
-
-
-Route::get('/sobre', function () {
-    return 'app super gestao sobre';
-});
-
-Route::get('/contato', function () {
-    return 'app super gestao contato';
-});
-*/
+use App\Http\Controllers\FornecedorController;
 
 Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
 Route::get('/sobre', [SobreController::class, 'sobre'])->name('site.sobre');
 Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::post('/contato', [ContatoController::class, 'contato'])->name('site.contato');
 Route::get('/login', function(){return 'login';})->name('site.login');
 
 Route::prefix('/app')->group(function(){
     Route::get('/clientes', function(){return 'clientes';})->name('app.clientes');
-    Route::get('/fornecedores', function(){return 'fornecedores';})->name('app.fornecedores');
+    Route::get('/fornecedores', [FornecedorController::class, 'index'])->name('app.fornecedores');
     Route::get('/produtos', function(){return 'produtos';})->name('app.produtos');
 });
 
