50c0c1c223d2fa15de46e4c9bee4b40febc641c3
alisson fredo <douglasalissonsf@gmail.com>
2021-08-02 23:18:27 -0300

    seção 9 - trabalhando com formulários - aula 120 entendendo o objeto request

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -6,8 +6,26 @@ use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function contato(){
-        var_dump($_POST);
+    public function contato(Request $request){
+        /*
+        *** dd(); apresenta um array na tela e finaliza a aplicação como se fosse um die; ***
+        dd($request);
+        */
+
+        /*
+        *** metodo all() recupera todos os atributos do array associativo ***
+        echo '<pre>';
+        print_r($request->all());
+        echo '</pre>';
+        */
+
+        /*
+        *** metodo input() apresenta somente o atributo desejado do array associativo ***
+        echo $request->input('nome');
+        echo '<br>';
+        echo $request->input('nome');
+        */
+
         return view('site.contato');
     }
 }
