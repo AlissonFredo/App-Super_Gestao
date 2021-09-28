473c30bba0f3afd17fddc805d14f4699aab2320d
alissonfredo <douglasalissonsf@gmail.com>
2021-09-27 21:48:33 -0300

    secao trabalhando com formularios - aulas: repopulando o formulario (request old input) parte 1

--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -2,11 +2,11 @@
 
 <form action="{{route('site.contato')}}" method="POST">
     @csrf
-    <input name="nome" type="text" placeholder="Nome" class="{{$classe}}">
+    <input name="nome" value="{{ old('nome') }}" type="text" placeholder="Nome" class="{{$classe}}">
     <br>
-    <input name="telefone" type="text" placeholder="Telefone" class="{{$classe}}">
+    <input name="telefone" value="{{ old('telefone') }}" type="text" placeholder="Telefone" class="{{$classe}}">
     <br>
-    <input name="email" type="text" placeholder="E-mail" class="{{$classe}}">
+    <input name="email" value="{{ old('email') }}" type="text" placeholder="E-mail" class="{{$classe}}">
     <br>
     <select name="motivo_contato" class="{{$classe}}">
         <option value="">Qual o motivo do contato?</option>
@@ -15,13 +15,16 @@
         <option value="3">Reclamação</option>
     </select>
     <br>
-    <textarea name="mensagem" class="{{$classe}}">Preencha aqui a sua mensagem</textarea>
+    <textarea name="mensagem" class="{{$classe}}">{{(old('mensagem') != '') ? old('mensagem') : 'Preencha aqui a sua mensagem'}}</textarea>
     <br>
     <button type="submit" class="{{$classe}}">ENVIAR</button>
 </form>
 
+
+<!--
 <div style="position: absolute; top: 0px; left: 0px; width: 100%; background: red;">
     <pre>
-        {{ print_r($errors) }}
+        { print_r($errors) }}
     </pre>
 </div>
+-->
