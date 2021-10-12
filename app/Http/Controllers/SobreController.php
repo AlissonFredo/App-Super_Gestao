576216ca0c03fd4f2a4756cf10cdf7dc8b893374
alissonfredo <douglasalissonsf@gmail.com>
2021-10-12 20:43:51 -0300

    secao middlewares - aulas: apelidando middlewares

--- app/Http/Controllers/SobreController.php
+++ app/Http/Controllers/SobreController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class SobreController extends Controller
 {
+    public function __construct() {
+        $this->middleware('log.acesso');
+    }
+
     public function sobre(){
         return view('site.sobre');
     }
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,7 +37,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\LogAcessoMiddleware::class,
+            //\App\Http\Middleware\LogAcessoMiddleware::class,
         ],
 
         'api' => [
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'log.acesso' => \App\Http\Middleware\LogAcessoMiddleware::class,
     ];
 }
