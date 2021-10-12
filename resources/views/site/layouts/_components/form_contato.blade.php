bdfad6ec96dfa908b3c3b34a97952e2026715c64
alissonfredo <douglasalissonsf@gmail.com>
2021-10-12 04:19:53 -0300

    secao trabalhando com formularios - aulas: customizando a apresentação dos erros de validação parte 2

--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -2,11 +2,36 @@
 
 <form action="{{route('site.contato')}}" method="POST">
     @csrf
-    <input name="nome" value="{{ old('nome') }}" type="text" placeholder="Nome" class="{{$classe}}">
+    <input 
+        name="nome" 
+        value="{{ old('nome') }}" 
+        type="text" 
+        placeholder="Nome" 
+        class="{{$classe}}"
+    >
+    {{$errors->has('nome') ? $errors->first('nome') : ''}}
     <br>
-    <input name="telefone" value="{{ old('telefone') }}" type="text" placeholder="Telefone" class="{{$classe}}">
+    <input 
+        name="telefone" 
+        value="{{ old('telefone') }}" 
+        type="text" 
+        placeholder="Telefone" 
+        class="{{$classe}}"
+    >
+    @if($errors->has('telefone'))
+        {{$errors->first('telefone')}}
+    @endif
     <br>
-    <input name="email" value="{{ old('email') }}" type="text" placeholder="E-mail" class="{{$classe}}">
+    <input 
+        name="email" 
+        value="{{ old('email') }}" 
+        type="text" 
+        placeholder="E-mail" 
+        class="{{$classe}}"
+    >
+    @if($errors->has('email'))
+        {{$errors->first('email')}}
+    @endif
     <br>
     <select name="motivo_contatos_id" class="{{$classe}}">
         <option value="">Qual o motivo do contato?</option>
@@ -19,8 +44,10 @@
             </option>
         @endforeach
     </select>
+    {{$errors->has('motivo_contatos_id') ? $errors->first('motivo_contatos_id') : ''}}
     <br>
     <textarea name="mensagem" class="{{$classe}}">{{(old('mensagem') != '') ? old('mensagem') : 'Preencha aqui a sua mensagem'}}</textarea>
+    {{$errors->has('mensagem') ? $errors->first('mensagem') : ''}}
     <br>
     <button type="submit" class="{{$classe}}">ENVIAR</button>
 </form>
