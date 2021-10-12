932a222c3b71a372f660f9d811d53e1086a5d327
alissonfredo <douglasalissonsf@gmail.com>
2021-10-12 03:37:41 -0300

    secao trabalhando com formularios - aulas: persistindo dados e redirecionando a rota

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -21,10 +21,10 @@ class ContatoController extends Controller
             'nome' => 'required|min:3|max:40',
             'telefone' => 'required',
             'email' => 'email',
-            'motivo_contato' => 'required',
+            'motivo_contatos_id' => 'required',
             'mensagem' => 'required|max:2000'
         ]);
-
-        //SiteContato::create($request->all());
+        SiteContato::create($request->all());
+        return redirect()->route('site.index');
     }
 }
--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -8,12 +8,12 @@
     <br>
     <input name="email" value="{{ old('email') }}" type="text" placeholder="E-mail" class="{{$classe}}">
     <br>
-    <select name="motivo_contato" class="{{$classe}}">
+    <select name="motivo_contatos_id" class="{{$classe}}">
         <option value="">Qual o motivo do contato?</option>
         @foreach ($motivo_contatos as $key => $motivo_contato)
             <option 
                 value="{{$motivo_contato->id}}" 
-                {{old('motivo_contato') == $motivo_contato->id ? 'selected' : ''}}
+                {{old('motivo_contatos_id') == $motivo_contato->id ? 'selected' : ''}}
             >
                 {{$motivo_contato->motivo_contato}}
             </option>
