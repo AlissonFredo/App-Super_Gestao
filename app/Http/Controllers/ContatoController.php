dba01b3ead406c4479b12ecc82686340fe0421e4
alissonfredo <douglasalissonsf@gmail.com>
2021-10-12 04:36:48 -0300

    secao trabalhando com formularios - aulas: custumizando as mensagens de feedback de validaçao

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -17,13 +17,24 @@ class ContatoController extends Controller
     public function salvar(Request $request){
 
         //realizar validação dos dados do formulário recebidos no request
-        $request->validate([
-            'nome' => 'required|min:3|max:40|unique:site_contatos',
-            'telefone' => 'required',
-            'email' => 'email',
-            'motivo_contatos_id' => 'required',
-            'mensagem' => 'required|max:2000'
-        ]);
+        $request->validate(
+            [
+                'nome' => 'required|min:3|max:40|unique:site_contatos',
+                'telefone' => 'required',
+                'email' => 'email',
+                'motivo_contatos_id' => 'required',
+                'mensagem' => 'required|max:2000'
+            ],
+            [
+                'nome.required' => 'O campo nome precisa ser preenchido',
+                'nome.min' => 'O campo nome precisa ter no minimo 3 caracteres',
+                'nome.max' => 'O campo nome precisa ter no maximo 40 caracteres',
+                'nome.unique' => 'O nome informado ja esta em uso',
+                'telefone.required' => 'O campo telefone precisa ser preenchido',
+                'required' => 'O campo deve ser preenchido'
+
+            ]
+        );
         SiteContato::create($request->all());
         return redirect()->route('site.index');
     }
