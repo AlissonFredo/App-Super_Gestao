b6adf787e6b9538e2498b57e02cab8324fa53355
alissonfredo <douglasalissonsf@gmail.com>
2021-10-12 20:35:55 -0300

    secao middlewares - aulas: Implementando um middleware para todas as rotas

--- app/Http/Controllers/SobreController.php
+++ app/Http/Controllers/SobreController.php
@@ -3,14 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Middleware\LogAcessoMiddleware;
 
 class SobreController extends Controller
 {
-    public function __construct() {
-        $this->middleware(LogAcessoMiddleware::class);
-    }
-
     public function sobre(){
         return view('site.sobre');
     }
