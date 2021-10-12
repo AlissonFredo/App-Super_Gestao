824fd35c04896e6c29ee242c97a6d5351c037229
alissonfredo <douglasalissonsf@gmail.com>
2021-10-12 20:05:43 -0300

    secao middlewares - aulas: Implementando middlewares no método construtor dos controllers

--- app/Http/Controllers/SobreController.php
+++ app/Http/Controllers/SobreController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Middleware\LogAcessoMiddleware;
 
 class SobreController extends Controller
 {
+    public function __construct() {
+        $this->middleware(LogAcessoMiddleware::class);
+    }
+
     public function sobre(){
         return view('site.sobre');
     }
