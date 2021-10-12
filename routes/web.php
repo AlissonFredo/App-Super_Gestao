9a6010d84dbae76aa123d330dd9f9e998bcbea04
alissonfredo <douglasalissonsf@gmail.com>
2021-10-12 19:32:14 -0300

    secao middlewarea - aulas: criando meu primeiro middleware

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,9 @@ use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\SobreController;
 use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\FornecedorController;
+use App\Http\Middleware\LogAcessoMiddleware;
 
-Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index')->middleware(LogAcessoMiddleware::class);
 Route::get('/sobre', [SobreController::class, 'sobre'])->name('site.sobre');
 Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
 Route::post('/contato', [ContatoController::class, 'salvar'])->name('site.contato');
